0612dbf2c3f002c6d67c24f52a86a4700c4403f7
Sergey <popravkin.s@mail.ru>
2021-03-05 14:52:30 +0300

    components1-4

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -10,10 +10,13 @@ function Navbar() {
           <Link to="/login">Войти</Link>
         </li>
         <li>
-          <Link to="/signup">Зареристрироваться</Link>
+          <Link to="/signup">Зарегистрироваться</Link>
         </li>
     <li>
       <Link to="/main">Главная</Link>
+    </li>
+    <li>
+      <Link to="/button">Кнопка</Link>
     </li>
       </ul>
     </nav>
