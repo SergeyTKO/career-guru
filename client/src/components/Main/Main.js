2dcc824b6a86c34850584891842d231d4c6d8396
Ekateraina <ekateraina@mail.ru>
2021-03-07 16:10:41 +0300

    styles

--- client/src/components/Main/Main.js
+++ client/src/components/Main/Main.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import AnswerCard from '../AnswerCard/AnswerCard'
 import QuestionCard from '../QuestionCard/QuestionCard'
-import styles from './Main.module.scss'
 
 
 function Main() {
