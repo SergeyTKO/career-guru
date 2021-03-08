1e00073682cbc01e7e95718bd5098903334036a5
Ekaterina Dolgopolova <ekateraina@mail.ru>
2021-03-08 12:13:10 +0300

    Delete Button.js

--- client/src/components/Button/Button.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-function Button({value,buttonHandler}) {
-  return (
-      <button type='button' value={value} onClick={(event) => buttonHandler(event)}>{value}</button>
-  );
-}
-
-export default Button;
