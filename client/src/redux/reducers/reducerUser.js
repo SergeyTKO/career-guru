0b8516d0524839d894cd5d45b1b73e14fdda4dfe
Sergey <oldskuul@mail.ru>
2021-03-08 23:22:34 +0300

    play logic v0.31

--- client/src/redux/reducers/reducerUser.js
+++ client/src/redux/reducers/reducerUser.js
@@ -23,7 +23,7 @@ export const reducerUser = (state = initialState, action) => {
         case REST_TO_FINISH:
             return {...state, result: {...state.result, numOfRestQuestions: state.result.numOfRestQuestions - 1}};
         case RESET_RESULTS:
-            return {...state, result: {...state.result, resultCounter: state.result.resultCounter = 0, resultCounter: state.result.resultCounter = 0}};
+            return {...state, result: {...state.result, resultCounter: state.result.resultCounter = 0, numOfRestQuestions: state.result.numOfRestQuestions = 0}};
         default:
             return state;
     }
