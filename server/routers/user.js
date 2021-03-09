d67155d93181a98742352ea6e899919831d2ed12
Sergey <popravkin.s@mail.ru>
2021-03-09 18:22:15 +0300

    backUser

--- server/routers/user.js
+++ server/routers/user.js
@@ -5,6 +5,7 @@ import Users from '../models/users.js'
 router.route('/user')
   .put(async (req, res) => {
     const { count, currentTest, _id } = req.body
+    
     const editUser = await Users.findById(_id)
     await editUser.updateOne({ score: count, solvedCards: currentTest})
     await editUser.save()
