84c1450b229f46df2c522bb69cb4b6dc00b46f24
Sergey <oldskuul@mail.ru>
2021-03-09 18:53:02 +0300

    fixes

--- client/src/components/Home/Home.js
+++ client/src/components/Home/Home.js
@@ -2,14 +2,17 @@ import styles from './Home.module.scss'
 import {Link} from 'react-router-dom'
 import {useDispatch} from "react-redux";
 import {resetResultsAC} from "../../redux/actionCreators";
-import { useEffect } from 'react';
-import { googleFetchAC } from '../../redux/thunk/googleFetchAC';
+import {useEffect} from 'react';
+import {googleFetchAC} from '../../redux/thunk/googleFetchAC';
 
-function Home(){
+function Home() {
     const dispatch = useDispatch();
-    dispatch(resetResultsAC());
-  
-    return(
+    useEffect(() => {
+        dispatch(resetResultsAC());
+    }, [dispatch])
+
+
+    return (
         <div className={styles.homePage}>
             <Link to='/cards'>Популярные вопросы на собеседовании</Link>
             <Link to='/test'>Пройти тестирование</Link>
