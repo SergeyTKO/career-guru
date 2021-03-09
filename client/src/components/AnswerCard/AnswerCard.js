1db68b6dc94c281fc1f9a40e7fec11a57653f7e6
Sergey <popravkin.s@mail.ru>
2021-03-09 11:31:19 +0300

    tttt

--- client/src/components/AnswerCard/AnswerCard.js
+++ client/src/components/AnswerCard/AnswerCard.js
@@ -2,14 +2,18 @@ import React from 'react';
 import Button from "../Button/Button";
 import styles from './AnswerCard.module.scss'
 
-function AnswerCard({answer, buttonHandler}) {
 
-    return (
+function AnswerCard({ answer, buttonHandler }) {
+
+
+
+
+  return (
     <div className={styles.answerCard}>
-          <Button btnValue={answer[0].answer} buttonHandler={buttonHandler}/>
-          <Button btnValue={answer[1].answer} buttonHandler={buttonHandler}/>
-          <Button btnValue={answer[2].answer} buttonHandler={buttonHandler}/>
-          <Button btnValue={answer[3].answer} buttonHandler={buttonHandler}/>
+      <Button btnValue={answer[0].answer} buttonHandler={buttonHandler} />
+      <Button btnValue={answer[1].answer} buttonHandler={buttonHandler} />
+      <Button btnValue={answer[2].answer} buttonHandler={buttonHandler} />
+      <Button btnValue={answer[3].answer} buttonHandler={buttonHandler} />
     </div>
   );
 }
