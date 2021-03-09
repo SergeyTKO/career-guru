b9a8356f598f5e01f20b684a32b21e6b95ec543c
Sergey <oldskuul@mail.ru>
2021-03-09 09:44:49 +0300

    play logic v0.4

--- client/src/components/Card/Card.js
+++ client/src/components/Card/Card.js
@@ -1,30 +1,48 @@
-import React from 'react';
+import React, {useState} from 'react';
 import QuestionCard from "../QuestionCard/QuestionCard";
 import AnswerCard from "../AnswerCard/AnswerCard";
 import styles from './Card.module.scss'
 import {useDispatch, useSelector} from "react-redux";
-import {answerAC, resultPlusOneAC, restToFinishAC} from "../../redux/actionCreators";
+import {answerAC, resultPlusOneAC, restToFinishAC, testProgressAC} from "../../redux/actionCreators";
 import {useHistory} from "react-router-dom";
 
-function Card({card}) {
+function Card({cards}) {
+    const [state, setState] = useState('');
     const history = useHistory()
     const dispatch = useDispatch()
     const result = useSelector(state => state.user.result.numOfRestQuestions)
+    const i = useSelector(state => state.user.result.testProgress)
     const buttonHandler = (event) => {
-        if (event.target.value === card.answer[0].answer) {
-            dispatch(answerAC(card))
+        if (event.target.value === cards[i].answer[0].answer) {
+            dispatch(answerAC(cards[i]))
             dispatch(resultPlusOneAC())
-            dispatch(restToFinishAC())
-        } else dispatch(restToFinishAC())
+            setState('Правильно')
+
+            setTimeout(() => {
+                dispatch(restToFinishAC())
+                dispatch(testProgressAC())
+                setState('');
+            }, 1500);
+        } else {
+            setState('Неправильно')
+
+            setTimeout(() => {
+                dispatch(restToFinishAC());
+                dispatch(testProgressAC())
+                setState('');
+            }, 1500)
+        }
     }
-        if ( result === 0) {
-            history.push('/results')
-        };
+    if (result === 0) {
+        history.push('/results')
+    }
+    ;
 
     return (
         <div className={styles.card}>
-                <QuestionCard question={card.question}/>
-                <AnswerCard answer={card.answer} buttonHandler={buttonHandler}/>
+            <div>{state}</div>
+            <QuestionCard question={cards[i].question}/>
+            <AnswerCard answer={cards[i].answer} buttonHandler={buttonHandler}/>
         </div>
     );
 }
