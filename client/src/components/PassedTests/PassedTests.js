55aea53d5175bea86c1525f12a9337fa80fea96f
Ekateraina <ekateraina@mail.ru>
2021-03-11 11:32:33 +0300

    tags

--- client/src/components/PassedTests/PassedTests.js
+++ client/src/components/PassedTests/PassedTests.js
@@ -1,17 +1,19 @@
 import React from 'react';
 import {useSelector} from "react-redux";
+import styles from './PassedTests.module.scss'
 
 
 function PassedTests(props) {
     const passedTests = useSelector(state => state.auth.user.solvedCards);
 
     return (
-        <div>
+        <div className={styles.passedTests}>
+        <h1>Вопросы, на которые ты уже знаешь ответ:</h1>
             {passedTests && passedTests.map(el =>
-                <div key={performance.now()}>
-                    <div>Вопрос: {el.question}</div>
-                    <div>Ответ: {el.answer[0].answer}</div>
-                    <div>Баллы: {el.count}</div>
+                <div key={performance.now()} className={styles.passedTestBlock}>
+                    <div><strong>Вопрос: </strong>{el.question}</div>
+                    <div><strong>Ответ: </strong>{el.answer[0].answer}</div>
+                    <div><strong>Баллы: </strong>{el.count}</div>
                 </div>
             )}
         </div>
--- client/src/components/QuestionCard/QuestionCard.js
+++ client/src/components/QuestionCard/QuestionCard.js
@@ -3,10 +3,16 @@ import styles from './QuestionCard.module.scss'
 
 function QuestionCard({question, index, result}) {
  
+result.currentTest.map(test=>console.log(test.tags[0]))
+
   return (
     <div className={styles.questionCard}>
      <div className={styles.questionCount}>{index + 1}/{result.currentTest.length}</div>
      <p>{question}</p>
+{
+  result.currentTest.tags && result.currentTest.map(test=><span>#{test.tags.map(tag=>tag)}</span>)
+}
+    
     </div>
   );
 }
