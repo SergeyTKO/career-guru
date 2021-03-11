5d33fb7c14ff14c3a416b8569d46102ea5a07c46
Sergey <oldskuul@mail.ru>
2021-03-11 15:42:46 +0300

    play logic v0.9/shuffle

--- client/src/components/Deck/Deck.js
+++ client/src/components/Deck/Deck.js
@@ -2,15 +2,19 @@ import React, {useState} from 'react'
 import {animated, interpolate} from 'react-spring'
 import {useSprings} from 'react-spring'
 import {useGesture} from 'react-use-gesture'
-import {useSelector} from "react-redux";
+import {useDispatch, useSelector} from "react-redux";
 import styles from '../App/App.module.scss'
+import {shuffleFunctionAC} from "../../redux/thunk/shuffleAC";
 
 const to = i => ({x: 0, y: i * -4, scale: 1, rot: -10 + Math.random() * 20, delay: i * 100})
 const from = i => ({x: 0, rot: 0, scale: 1.5, y: -1000})
 const trans = (r, s) => `perspective(1500px) rotateX(15deg) rotateY(${r / 10}deg) rotateZ(${r / 10}deg) scale(${s})`
 
 function Deck() {
-    const cards = useSelector(state => state.admin.cards);
+    const dispatch = useDispatch();
+    const oldcards = useSelector(state => state.admin.cards);
+    dispatch(shuffleFunctionAC(oldcards))
+    const cards = useSelector(state => state.user.result.shuffle);
     const [gone] = useState(() => new Set())
     const [props, set] = useSprings(cards.length, i => ({...to(i), from: from(i)}))
     const bind = useGesture(({args: [index], down, delta: [xDelta], distance, direction: [xDir], velocity}) => {
